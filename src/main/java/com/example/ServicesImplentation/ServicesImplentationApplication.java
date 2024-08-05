b01d367874fed5aa75f4f7889172b36dbde600e6
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-05 22:10:04 +0530

    Deployment

---
+++ src/main/java/com/example/ServicesImplentation/ServicesImplentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServicesImplentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesImplentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesImplentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServicesImplentation/ServicesImplentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServicesImplentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesImplentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
