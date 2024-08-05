58d2b97e0e8cbfca217f31507e5bca0fd7076fd7
Koushal Kumar <koushalkumar2224@gmail.com>
2024-08-05 23:03:32 +0530

    Exceptions

---
+++ src/main/java/com/example/ServicesImplentation/Exceptions/IdNotFounfException.java
@@ -0,0 +1,7 @@
+package com.example.ServicesImplentation.Exceptions;
+
+public class IdNotFounfException extends RuntimeException{
+    public IdNotFounfException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/ServicesImplentation/Exceptions/NameNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.ServicesImplentation.Exceptions;
+
+public class NameNotFoundException extends RuntimeException{
+    public NameNotFoundException(String message){
+        super(message);
+    }
+}
